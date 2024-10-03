83ee8358363ce6822cda4659f1136a1ac2b4660a
Pasquale Muraca <pasqualemuraca2003@gmail.com>
2024-10-03 23:20:31 +0200

    aggiunta grafica checkbox biometrics

--- TODO.md
+++ TODO.md
@@ -3,11 +3,10 @@ forzare portrait
 adattamento light/dark theme
 
 ## primo avvio
-1. pagina di login con logo e due textbox e un bottone (email e password) (non mostrare la navbar)
-2. quando il login è effettuato con successo bisogna salvare da qualche parte email e password inserite così il prossimo avvio fa la richiesta di autenticazione in automatico
+1. pagina di login con logo e due textbox e un bottone (email e password) (non mostrare la navbar bassa)
+potrebbe essere utile una checkbox con scritto "ABILITA LOGIN RAPIDO CON DATI BIOMETRICI"
+3. quando il login è effettuato con successo bisogna salvare da qualche parte email e password inserite così il prossimo avvio fa la richiesta di autenticazione in automatico
    (si crea tuttavia una vulnerabilità)
-3. ai prossimi avvii l'app farà l'autenticazione con le credenziali salvate, previa autenticazione biometrica
-
-
+4. ai prossimi avvii l'app farà l'autenticazione con le credenziali salvate, previa autenticazione biometrica
 
 invio notifiche quando si effettua una transazione (leggermente fine a se stesso)
--- app/src/main/java/com/example/app_mensa/LoginActivity.java
+++ app/src/main/java/com/example/app_mensa/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.app_mensa;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -11,31 +12,35 @@ import androidx.appcompat.app.AppCompatActivity;
 public class LoginActivity extends AppCompatActivity {
 
     private EditText emailEditText, passwordEditText;
+    private CheckBox biometricCheckbox;
     private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        associateUI();
+    }
 
+    private void associateUI() {
         emailEditText = findViewById(R.id.email);
         passwordEditText = findViewById(R.id.password);
+        biometricCheckbox = findViewById(R.id.biometricCheckbox);
         loginButton = findViewById(R.id.loginButton);
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email = emailEditText.getText().toString();
-                String password = passwordEditText.getText().toString();
-
-                if (email.isEmpty() || password.isEmpty()) {
-                    Toast.makeText(LoginActivity.this, "Riempi tutti i campi", Toast.LENGTH_SHORT).show();
-                } else if (email.equals("admin@example.com") && password.equals("admin123")) {
-                    Toast.makeText(LoginActivity.this, "Login effettuato con successo", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(LoginActivity.this, "Email o password errate", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        loginButton.setOnClickListener(view -> loginAction());
     }
+
+    private void loginAction() {
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(LoginActivity.this, "Riempi tutti i campi", Toast.LENGTH_SHORT).show();
+        } else if (email.equals("admin@example.com") && password.equals("admin123")) {
+            Toast.makeText(LoginActivity.this, "Login effettuato con successo", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(LoginActivity.this, "Email o password errate", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
